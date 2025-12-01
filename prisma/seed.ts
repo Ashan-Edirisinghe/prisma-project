dd39732baf5af6d8aff14d239c5401b169bd191a
Ashan-Edirisinghe <ashanedirisingheugc@gmail.com>
2025-12-01 13:39:23 +0530

    edit: new prisma 7 setup a db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,30 @@
-import { PrismaClient } from "@prisma/client/extension";    
-const prisma = new PrismaClient();
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-async function seed() {}
+async function seed() {
+    const connectionString = process.env.DATABASE_URL!;
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    const prisma = new PrismaClient({ adapter });
+    
+    try {
+        await prisma.user.createMany({
+            data: [
+                { email: "ashan@gmailcom", name: "Ashan" },
+                { email: "kumar@gmailcom", name: "Kumar" },
+                { email: "nimal@gmailcom", name: "Nimal" }
+            ]
+        });
+        console.log("Seeded 3 users successfully");
+    } catch (error) {
+        console.error("Error seeding data:", error);
+        throw error;
+    } finally {
+        await prisma.$disconnect();
+        await pool.end();
+    }
+}
+
+seed();
